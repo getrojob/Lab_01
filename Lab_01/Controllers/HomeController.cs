4098c7269755d23cd6064511ea3159ccf30af6e9
Getulio Rodrigues <getrojob@gmail.com>
2019-03-10 12:24:14 -0300

    Mini projeto concluido

---
+++ Lab_01/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using Lab_01.Models;
+
+namespace Lab_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Portfolio()
+        {
+
+            string pathVirtual = "~/imagens/Portfolio/";
+            string pathFisico = Server.MapPath(pathVirtual);
+            string[] arquivos = Directory.GetFiles(pathFisico);
+            List<string> imageUrlList = new List<string>();
+
+            foreach (string arquivo in arquivos)
+            {
+                string nomeArquivo = Path.GetFileName(arquivo);
+                string imageUrl = Url.Content(pathVirtual + nomeArquivo);
+
+                imageUrlList.Add(imageUrl);
+            }
+
+            ViewBag.ImageUrlList = imageUrlList;
+            return View();
+        }
+
+        public ActionResult Contato()
+        {
+            ViewBag.Concluido = false;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contato(ContatoViewModel contato)
+        {
+            string pathVirtual = "~/contato.txt";
+
+            string pathFisico = Server.MapPath(pathVirtual);
+
+            using (var sw = new StreamWriter(pathFisico, true))
+            {
+                sw.WriteLine(contato.Nome);
+                sw.WriteLine(contato.Email);
+                sw.WriteLine(contato.Mensagem);
+                sw.WriteLine(DateTime.Now);
+                sw.WriteLine();
+            }
+            ViewBag.Concluido = true;
+            return View();
+        }
+
+    }
+}
---
+++ Lab_01/Models/ContatoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab_01.Models
+{
+    public class ContatoViewModel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
